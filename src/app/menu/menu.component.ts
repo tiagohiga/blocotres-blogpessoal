007e973731a46ccf2ffb48fc6951098470fd5d3a
Tiago Higa <higa_higa@outlook.com>
2021-06-30 13:59:39 -0300

    Implementando a tela de Inicio

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  nomeUsuario = environment.nomeUsuario
+  fotoUsuario = environment.fotoUsuario
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  sair(){
+    this.router.navigate(['/entrar'])
+    environment.fotoUsuario = ''
+    environment.idUsuario = 0
+    environment.nomeUsuario = ''
+    environment.tokenUsuario = ''
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -19,5 +20,15 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogdohiga.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if(environment.tokenUsuario != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
